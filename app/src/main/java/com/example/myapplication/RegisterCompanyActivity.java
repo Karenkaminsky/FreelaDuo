6ce3e93a86e120adfe8685f0da2b34f0c8cd5bf6
Karen Kaminsky <karenkaminskysilvaa@gmail.com>
2026-08-22 22:17:09 -0300

    refactor(company): sync layout view IDs with RegisterCompanyActivity

--- app/src/main/java/com/example/myapplication/RegisterCompanyActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterCompanyActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.myapplication.R;
 
 public class RegisterCompanyActivity extends AppCompatActivity {
 
